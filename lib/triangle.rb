f9fc824084b59dad0dc19f9f6af98b51ee9c4c21
Amanda Rolon <abrolon87@gmail.com>
2020-01-16 16:28:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,6 @@
 class Triangle
 
-  attr_accessor :side_1, :side_2, :side_3 
+  attr_accessor :side_1, :side_2, :side_3
 
   def initialize(side_1, side_2, side_3)
     @side_1 = side_1
@@ -8,20 +8,20 @@ class Triangle
     @side_3 = side_3
   end
 
-  def kind 
+  def kind
     validate_triangle  #calls validate_triangle and then compares sides to find which kind of triangle it is
     if side_1 == side_2 && side_2 == side_3
       :equilateral
     elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
       :isosceles
-    else                  
+    else
       :scalene
     end
   end
 
   def validate_triangle
-    real_triangle = [(side_1 + side_2 > side_3), (side_1 + side_3 > side_2), (side_2 + side_3 > side_1)] #possible triangles 
-    [side_1, side_2, side_3].each do |side| #iterate over sides to validate a triangle 
+    real_triangle = [(side_1 + side_2 > side_3), (side_1 + side_3 > side_2), (side_2 + side_3 > side_1)] #possible triangles
+    [side_1, side_2, side_3].each do |side| #iterate over sides to validate a triangle
       real_triangle << false if side <= 0  # 
     raise TriangleError if real_triangle.include?(false)
     end
